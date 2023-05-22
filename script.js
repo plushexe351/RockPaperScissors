6ab9c7259b77028f86bde37e196504b195ddb920
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-05-22 23:27:13 +0530

    bug fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-let chances = 1;
+let chances = 0;
 let win_msg;
 let playerScore = 0, computerScore = 0;
 const choices = document.querySelectorAll('.choices img');
@@ -12,45 +12,42 @@ const score2 = document.querySelector('#computerScore');
 const score1 = document.querySelector('#playerScore');
 const result = document.querySelector('.result');
 const winner = document.querySelector('.winner');
-round.innerHTML = `0/5`;
+round.innerHTML = `${chances}/5`;
 
 choices.forEach(choice => {
     choice.addEventListener('click', () => {
+        chances++;
 
         round.innerHTML = `${chances}/5`;
+
+        let playerSelection = choice.getAttribute("id").toUpperCase();
+
+        let computerSelection = getComputerChoice().toUpperCase();
+
+        document.querySelector('.computerchoice').innerHTML =
+            `<img src="images/rps_${computerSelection.toLowerCase()}.png">`;
+
+
+        if (playRound(playerSelection, computerSelection) == 'Computer Wins ! Better luck next time...')
+            score2.innerHTML = ++computerScore;
+
+        if (playRound(playerSelection, computerSelection) == 'You Win !')
+            score1.innerHTML = ++playerScore;
+
+        if (playerScore > computerScore)
+            win_msg = "You Win!";
+
+        else if (computerScore > playerScore)
+            win_msg = "You Lost";
+
+        else if (computerScore == playerScore)
+            win_msg = "Tie!";
 
         if (chances == 5) {
             result.style.display = "flex";
             winner.textContent = win_msg;
         }
 
-        else {
-
-            let playerSelection = choice.getAttribute("id").toUpperCase();
-
-            let computerSelection = getComputerChoice().toUpperCase();
-
-            document.querySelector('.computerchoice').innerHTML =
-                `<img src="images/rps_${computerSelection.toLowerCase()}.png">`;
-
-            ++chances;
-
-            if (playRound(playerSelection, computerSelection) == 'Computer Wins ! Better luck next time...')
-                score2.innerHTML = ++computerScore;
-
-            if (playRound(playerSelection, computerSelection) == 'You Win !')
-                score1.innerHTML = ++playerScore;
-
-            if (playerScore > computerScore)
-                win_msg = "You Win!";
-
-            else if (computerScore > playerScore)
-                win_msg = "You Lost";
-
-            else if (computerScore == playerScore)
-                win_msg = "Tie!";
-
-        }
     })
 });
 
@@ -58,7 +55,7 @@ document.querySelector('.reset').addEventListener('click', () => {
     setTimeout(() => {
         result.style.display = "none";
         round.innerHTML = "0/5";
-        chances = 2, playerScore = 0, computerScore = 0;
+        chances = 0, playerScore = 0, computerScore = 0;
         score1.textContent = 0;
         score2.textContent = 0;
     }, 400);
