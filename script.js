48b5b1c3c4d70a60c05b0933430c98f75c084bbe
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-08-02 16:18:25 +0530

    Fixed an issue where game was playable after 5 rounds without reset

--- script.js
+++ script.js
@@ -1,98 +1,79 @@
-function getComputerChoice() {
-    const choice = ['Rock', 'Paper', 'Scissor'];
-    return choice[Math.floor(Math.random() * 3)];
-}
+
+const choices = document.querySelectorAll('.choices img');
+const roundElement = document.getElementById('round');
+const score1Element = document.querySelector('#playerScore');
+const score2Element = document.querySelector('#computerScore');
+const resultElement = document.querySelector('.result');
+const winnerElement = document.querySelector('.winner');
+const logElement = document.getElementById('log');
 
 let chances = 0;
 let win_msg;
+let playable = true;
 let playerScore = 0, computerScore = 0;
-const choices = document.querySelectorAll('.choices img');
-const round = document.getElementById('round');
-const score2 = document.querySelector('#computerScore');
-const score1 = document.querySelector('#playerScore');
-const result = document.querySelector('.result');
-const winner = document.querySelector('.winner');
-const log = document.querySelector('.log');
-round.innerHTML = `${chances}/5`;
+
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissor'];
+    return choices[Math.floor(Math.random() * 3)].toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie !';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissor') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissor' && computerSelection === 'paper')
+    ) {
+        return 'You Win !';
+    } else {
+        return 'Computer Wins ! Better luck next time...';
+    }
+}
 
 choices.forEach(choice => {
     choice.addEventListener('click', () => {
-        chances++;
+        if (playable) {
+            chances++;
+            roundElement.innerHTML = `${chances}/5`;
 
-        round.innerHTML = `${chances}/5`;
+            const playerSelection = choice.getAttribute('id').toLowerCase();
+            const computerSelection = getComputerChoice();
 
-        let playerSelection = choice.getAttribute("id").toUpperCase();
+            document.querySelector('.computerchoice').innerHTML =
+                `<img src="images/rps_${computerSelection}.png">`;
 
-        let computerSelection = getComputerChoice().toUpperCase();
+            const roundResult = playRound(playerSelection, computerSelection);
 
-        document.querySelector('.computerchoice').innerHTML =
-            `<img src="images/rps_${computerSelection.toLowerCase()}.png">`;
+            if (roundResult === 'Computer Wins ! Better luck next time...') {
+                score2Element.innerHTML = ++computerScore;
+                logElement.textContent = `You lose this round ! ${computerSelection} beats ${playerSelection}`;
+            } else if (roundResult === 'You Win !') {
+                score1Element.innerHTML = ++playerScore;
+                logElement.textContent = `You win this round ! ${playerSelection} beats ${computerSelection}`;
+            } else {
+                logElement.textContent = `This round's a Tie`;
+            }
 
+            win_msg = (playerScore > computerScore) ? 'You Win!' : (computerScore > playerScore) ? 'You Lost' : 'Tie!';
 
-        if (playRound(playerSelection, computerSelection) == 'Computer Wins ! Better luck next time...') {
-            score2.innerHTML = ++computerScore;
-            log.textContent = `You lose this round ! ${computerSelection} beats ${playerSelection}`;
+            if (chances === 5) {
+                playable = false;
+                resultElement.style.display = 'flex';
+                winnerElement.textContent = win_msg;
+            }
         }
-        else if (playRound(playerSelection, computerSelection) == 'You Win !') {
-            score1.innerHTML = ++playerScore;
-            log.textContent = `You win this round ! ${playerSelection} beats ${computerSelection}`;
-        }
-        else log.textContent = `This round's a Tie`;
-
-        if (playerScore > computerScore)
-            win_msg = "You Win!";
-
-        else if (computerScore > playerScore)
-            win_msg = "You Lost";
-
-        else if (computerScore == playerScore)
-            win_msg = "Tie!";
-
-        if (chances == 5) {
-            result.style.display = "flex";
-            winner.textContent = win_msg;
-        }
-
-    })
+    });
 });
 
 document.querySelector('.reset').addEventListener('click', () => {
     setTimeout(() => {
-        result.style.display = "none";
-        round.innerHTML = "0/5";
+        resultElement.style.display = 'none';
+        roundElement.innerHTML = '0/5';
+        playable = true;
         chances = 0, playerScore = 0, computerScore = 0;
-        score1.textContent = 0;
-        score2.textContent = 0;
-        log.textContent = "";
+        score1Element.textContent = 0;
+        score2Element.textContent = 0;
+        logElement.textContent = '';
     }, 400);
-})
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "ROCK" && computerSelection == "ROCK")
-        return 'Tie !';
-
-    if (playerSelection == "ROCK" && computerSelection == "PAPER")
-        return 'Computer Wins ! Better luck next time...'
-
-    if (playerSelection == "ROCK" && computerSelection == "SCISSOR")
-        return 'You Win !'
-
-    if (playerSelection == "PAPER" && computerSelection == "ROCK")
-        return 'You Win !'
-
-    if (playerSelection == "PAPER" && computerSelection == "PAPER")
-        return 'Tie !';
-
-    if (playerSelection == "PAPER" && computerSelection == "SCISSOR")
-        return 'Computer Wins ! Better luck next time...';
-
-    if (playerSelection == "SCISSOR" && computerSelection == "ROCK")
-        return 'Computer Wins ! Better luck next time...';
-
-    if (playerSelection == "SCISSOR" && computerSelection == "PAPER")
-        return 'You Win !';
-
-    if (playerSelection == "SCISSOR" && computerSelection == "SCISSOR")
-        return 'Tie !';
-
-}
+});
